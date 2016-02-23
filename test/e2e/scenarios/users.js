4ca0d58c41b30466235a1d33ce52b8dfa3188788
John Hardy <jh@lagado.com>
2016-02-24 00:55:09 +1100

    e2e testing

---
+++ test/e2e/scenarios/users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var UserList    = require('../pages/UserList.js');
+var UserDetails = require('../pages/UserDetails.js');
+var ContactUser = require('../pages/ContactUser.js');
+
+describe('my app', function() {
+
+  var users   = new UserList();
+  var details = new UserDetails();
+  var contact = new ContactUser();
+
+  beforeEach(function() {
+    users.loadAll();
+  });
+
+  it('should load a list of users', function() {
+    expect(users.count()).toBeGreaterThan(1);
+  });
+
+  describe('selecting a user', function() {
+
+    beforeEach(function() {
+      return details.contactUser();
+    });
+
+    it('should set focus on first button in the bottomsheet view', function() {
+      expect( contact.buttons().count() ).toBe(4);
+      expect( contact.buttons().get(1).getText() ).toEqual( 'TWITTER' );
+      expect( contact.focusedButton().getText() ).toEqual( 'PHONE' );
+    });
+
+  });
+});
+
